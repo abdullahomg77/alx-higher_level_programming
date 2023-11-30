71367e110e76836b2fbb10e1b266e475a089d249
abdullahomg77 <abdullahomg77@gmail.com>
2023-11-30 02:27:01 -0800

    task 8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,8 +2,6 @@
 def islower(c):
     for i in range(97, 123):
         if c == i:
-            print("{} is lower".format(c))
             return True
         else:
-            print("{} is upper".format(c))
             return False
