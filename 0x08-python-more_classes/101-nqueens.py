60d8d174cb13e8f280451ab0bc86583dac1e64aa
abdullahomg77 <abdullahomg77@gmail.com>
2024-01-28 11:24:17 -0800

    3

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle.
+"""Solves the N-queens puzzle"""
 
-Determines all possible solutions to placing N
-N non-attacking queens on an NxN chessboard.
-
-Example:
-    $ ./101-nqueens.py N
-
-N must be an integer greater than or equal to 4.
-
-Attributes:
-    board (list): A list of lists representing the chessboard.
-    solutions (list): A list of lists containing solutions.
-
-Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
-where `r` and `c` represent the row and column, respectively, where a
-queen must be placed on the chessboard.
-"""
 import sys
 
 
@@ -120,9 +104,9 @@ def recursive_solve(board, row, queens, solutions):
             tmp_board[row][c] = "Q"
             xout(tmp_board, row, c)
             solutions = recursive_solve(tmp_board, row + 1,
-                    queens + 1, solutions)
+                                        queens + 1, solutions)
 
-            return (solutions)
+    return (solutions)
 
 
 if __name__ == "__main__":
